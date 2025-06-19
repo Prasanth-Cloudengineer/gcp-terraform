43b1e2118d7cd8af4987690bfe099be03fc8fb56
Prasanth-cloudengineer <prasanth.cloudengineer@gmail.com>
2025-06-19 21:37:13 +0530

    putty-13

--- putty.tf
+++ putty.tf
@@ -1,7 +1,7 @@
 
 resource "google_compute_instance" "vm_default" {
   project      = "prj-github-prasanth"  
-  name         = "vm-1"
+  name         = "putty-vm"
   machine_type = "e2-micro"
   zone         = "us-central1-a"
 
@@ -25,7 +25,7 @@ EOT
 
 metadata = {
   ssh-keys = <<EOT
-prasanth=ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCQDxJ4oYAOn9y7Ik0qTT0s+STS2KFjSd+M5O2kBMUnqnTj8rVgUQ4BGGAtt2f55zECPTwF3Z3IJ2EPSH+51+nWCVP5N7jXg4jpmAAvyaIaoGtdjxDgTqGQ8qP/lOwoxKxHiRsnLIHYvGczE1Dxr9AJFzbkiiJxA86+4pKakEi1C5yU4YVcyITigTQw0WICEwQa78BFwhCVkxBRAinkTipGbkrHW06uhd3TJf9itezf+Nkzf1+e5RtvS0kvTwoT5/Sjg42sm0KpWgVRRw/cxXmzSt5cNNYjNizWSMftbuLvHd56EC19RDGn01l2k84uV21KFjRJW+Vt+kqj2MVjw2Jf rsa-key-20250619
+prasanth:ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCQDxJ4oYAOn9y7Ik0qTT0s+STS2KFjSd+M5O2kBMUnqnTj8rVgUQ4BGGAtt2f55zECPTwF3Z3IJ2EPSH+51+nWCVP5N7jXg4jpmAAvyaIaoGtdjxDgTqGQ8qP/lOwoxKxHiRsnLIHYvGczE1Dxr9AJFzbkiiJxA86+4pKakEi1C5yU4YVcyITigTQw0WICEwQa78BFwhCVkxBRAinkTipGbkrHW06uhd3TJf9itezf+Nkzf1+e5RtvS0kvTwoT5/Sjg42sm0KpWgVRRw/cxXmzSt5cNNYjNizWSMftbuLvHd56EC19RDGn01l2k84uV21KFjRJW+Vt+kqj2MVjw2Jf rsa-key-20250619
 EOT
 }
 
