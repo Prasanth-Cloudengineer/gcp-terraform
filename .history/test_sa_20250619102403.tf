45347e10271b344ef02ff00104d7c61861e1a866
Prasanth-cloudengineer <prasanth.cloudengineer@gmail.com>
2025-06-19 10:26:55 +0530

    fix sa errors

--- .history/test_sa_20250619102403.tf
+++
@@ -1,6 +0,0 @@
-
-resource "google_service_account" "sa" {
-  account_id   = "tfe-test-service account"
-  project      = "prj-github-prasanth"
-  display_name = "TFE Test Service Account"
-}
--- test_sa.tf
+++ test_sa.tf
@@ -1,6 +1,6 @@
 
 resource "google_service_account" "sa" {
-  account_id   = "tfe-test-service account"
+  account_id   = "tfe-test-service-account"
   project      = "prj-github-prasanth"
   display_name = "TFE Test Service Account"
 }
