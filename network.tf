bcc476695b079fcfad7a3d2e0d50b35f486d0355
Prasanth-cloudengineer <prasanth.cloudengineer@gmail.com>
2025-06-19 09:57:40 +0530

    fix errors

--- network.tf
+++ network.tf
@@ -1,7 +1,7 @@
-
+/*
 resource "google_compute_network" "vpc_network" {
   project                 = "prj-github-prasanth"  
   name                    = "tfe-test-vpc"
   auto_create_subnetworks = true
-  depends_on = [google_project_service.enable_apis]
 }
+*/
