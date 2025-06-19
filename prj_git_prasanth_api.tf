53a3401e2ca39a44f41fc2c3aebdf998c33ba743
Prasanth-cloudengineer <prasanth.cloudengineer@gmail.com>
2025-06-19 08:39:26 +0530

    fix errors

--- prj_git_prasanth_api.tf
+++ prj_git_prasanth_api.tf
@@ -1,4 +1,5 @@
 
+#vIpugfW5ILILfQ.atlasv1.r0qyhEezj3BgAEzVzy8ON9wk8DlMaxDKDLWdAmiXDGz6eY0FH5aI5k0UCHEfFgGz7ZU
 resource "google_project_service" "enable_apis" {
   for_each = toset(var.api_services)
 
--- terrafrom.tfvars
+++ terrafrom.tfvars
@@ -1,5 +1,5 @@
 
-project_id = "your-gcp-project-id"
+project_id = "prj-github-prasanth"
 region     = "us-central1"
 
 # Optionally override the list of APIs
