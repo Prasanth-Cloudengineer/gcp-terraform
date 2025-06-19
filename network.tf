aabeb344eeb3cfe0bb14d327bfefc455628d7a1c
Prasanth-cloudengineer <prasanth.cloudengineer@gmail.com>
2025-06-19 10:42:13 +0530

    destroy network

--- network.tf
+++ network.tf
@@ -1,6 +1,7 @@
-
+/*
 resource "google_compute_network" "vpc_network" {
   project                 = "prj-github-prasanth"  
   name                    = "tfe-test-vpc"
-  auto_create_subnetworks = true
+  auto_create_subnetworks = false
 }
+*/
